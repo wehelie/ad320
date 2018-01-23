f93c8ca88dd5a1d20ac317c115fcdc1412ceaa8f
Leyth <3928442+wehelie@users.noreply.github.com>
2018-01-23 13:38:09 -0800

    Create header.php

---
+++ homework/homework3/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>The Paint Company</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ********************************************************************************* 
+        
+        A painting company has determined that for every 115 square feet of wall space, 
+        one gallon of paint and eight hours of labor will be required. 
+        The company charges $20.00 per hour for labor. Write a program that asks
+        the user to enter the square feet of wall space  to be painted and the price of
+        the paint per gallon in a HTML form. The program should display the following data:
+        
+        • The number of gallons of paint required
+        • The hours of labor required
+        • The cost of the paint
+        • The labor charges
+        • The total cost of the paint job 
+    ***********************************************************************************-->
+
+    <div class="container">
